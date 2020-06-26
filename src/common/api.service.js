49fc3202224a7e3266da88d34f858f0c8dc1fa9c
Observatorio - Satelite Noticias <obstechlab@gmail.com>
2020-06-26 10:37:08 -0400

    api service fix and test buttons

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -10,16 +10,18 @@ function handleResponse(response) {
   }
 }
 //SE PODRIA USAR EL PAQUETE AXIOS
-function apiService(endpoint, method, credentials, data ) {
+function apiService(endpoint, method, data,credentials ) {
+
   const config = {
     method: method || "GET",
     body: data !== undefined ? JSON.stringify(data) : null,
     headers: {
       "content-type": "application/json",
       "X-CSRFTOKEN": CSRF_TOKEN,
-      "authorization": credentials
+      "Authorization": credentials !== undefined ? "Token "+credentials : null,
     }
   };
+  console.log(config)
   return fetch(endpoint, config)
     .then(handleResponse)
     .catch(error => console.log(error));
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,11 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h1 class="theStylish">This is an about page2</h1>
-    <button @click="callingAxios">un boton mas</button>
+        <h1 class="theStylish">{{credentials}}</h1>
+
+    <button @click="callingAxios">Hacer login</button>
+    <button @click="callingAxios0">Crear usuario</button>
+    <button @click="callingAxios2">Sacar noticias</button>
 
   </div>
 </template>
@@ -15,11 +19,44 @@ export default {
   components: {
     
   },
+  data() {
+    return {
+      credentials: null
+    };
+  },
   methods:{
+     callingAxios0(){
+      alert("me apretaron 0  owo")
+      let endpoint = "https://satelite-de-noticias.herokuapp.com/api/rest-auth/registration/"
+      apiService(endpoint, "POST", {
+          "username":"admin10",
+          "email":"admin10@gmail.com",
+          "password1":"passwordodes2",
+          "password2":"passwordodes2",
+          "is_staff":false
+        }).then(data => {
+          alert(data.key)
+          this.credentials = data.key
+      });
+    
+    },
     callingAxios(){
       alert("me apretaron owo")
-      apiService('https://satelite-de-noticias.herokuapp.com/api/news/','GET','511689fa91844a15910736215dcf0ac07cbce4ad').then(data => {
-        console.log(data)
+      let endpoint = "https://satelite-de-noticias.herokuapp.com/api/rest-auth/login/"
+      apiService(endpoint, "POST", {
+          username: "admin10",
+	        password:"passwordodes2"
+        }).then(data => {
+          alert(data.key)
+          this.credentials = data.key
+      });
+    
+    },
+    callingAxios2(){
+      alert("me apretaron2 owo")
+      let endpoint2 = "https://satelite-de-noticias.herokuapp.com/api/news/"
+      apiService(endpoint2, "GET", undefined, this.credentials).then(data => {
+          console.log(data)
       });
     
     }
